0a459ef2d043aa45b3f8f9e4eb87e3f002adb348
Nikita <ujn2002ujn@mail.ru>
2023-03-19 14:05:32 +0300

    #11 Функция базовой мутации

--- oneParentBinaryOperators.cpp
+++ oneParentBinaryOperators.cpp
@@ -1,1 +1,14 @@
 #include "oneParentBinaryOperators.h"
+
+std::vector<bool> basicMutation(std::vector<bool> parent)
+{
+    if (parent.size() == 0)
+    {
+        std::cout << "Размер вектора равен нулю";
+        exit(1);
+    }
+    int geneNumber = rand() % (parent.size() - 1);
+    parent.at(geneNumber) = parent.at(geneNumber);
+
+    return parent;
+}
--- oneParentBinaryOperators.h
+++ oneParentBinaryOperators.h
@@ -1,1 +1,10 @@
 #include "config.h"
+
+/**
+ * Функция выполняет базовую мутация
+ *
+ * @param parent -родительская особь
+ *
+ * @return Возвращает мутировшую особь
+ */
+std::vector<bool> basicMutation(std::vector<bool> parent);
