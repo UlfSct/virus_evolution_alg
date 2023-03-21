3a607a95313f66f65d10890d57698693b2a68142
dicramble <umri-ot-spida@ya.ru>
2023-03-21 10:30:56 +0300

    Функция транслокации

--- oneParentBinaryOperators.cpp
+++ oneParentBinaryOperators.cpp
@@ -1,1 +1,41 @@
 #include "oneParentBinaryOperators.h"
+#include <vector>
+
+std::vector<bool> translocation(std::vector<bool> parent)
+{
+    srand(time(NULL));
+    if (parent.size() == 0)
+    {
+        std::cout << "Размер вектора равен нулю";
+        exit(1);
+    }
+
+    int genePositionFirst, genePositionLast;
+    
+    do
+    {
+        genePositionFirst = rand() % parent.size() - 1;
+        genePositionLast = rand() % parent.size();
+
+    } while (genePositionFirst > genePositionLast || genePositionFirst <= 0 || genePositionFirst == genePositionLast);
+
+    std::vector<bool> tmpParent;
+
+    for (int genePosition = genePositionFirst; genePosition <= genePositionLast; genePosition++)
+    {
+        tmpParent.push_back(parent[genePosition]);
+    }
+
+    for (int i = 0; i < tmpParent.size(); i++) {
+        std::cout << tmpParent[i] << " ";
+    }
+
+    std::reverse(tmpParent.begin(), tmpParent.end());
+
+    for (int geneParentPosition = genePositionFirst, geneTmpParentPosition = 0; geneTmpParentPosition < tmpParent.size(); geneParentPosition++, geneTmpParentPosition++)
+    {
+        parent[geneParentPosition] = tmpParent[geneTmpParentPosition];
+    }
+
+    return parent;
+}
--- oneParentBinaryOperators.h
+++ oneParentBinaryOperators.h
@@ -1,1 +1,11 @@
 #include "config.h"
+
+/*
+* Функция выполняет операцию транслокации
+*
+* @param parent -родительская особь
+*
+* @return Возвращает мутировшую особь
+*/
+
+std::vector<bool> translocation(std::vector<bool> parent);
