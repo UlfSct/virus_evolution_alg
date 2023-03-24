22a5c07e1d1a09b6efeb1ba477add2cb2ef01203
Nikita <ujn2002ujn@mail.ru>
2023-03-24 21:06:43 +0300

    #40 Функция базового кроссовера

--- twoParentsBinaryOperators.cpp
+++ twoParentsBinaryOperators.cpp
@@ -1,1 +1,44 @@
 #include "twoParentsBinaryOperators.h"
+
+std::vector<std::vector<bool>> basicCrossover(std::vector<bool> firstParent, std::vector<bool> secondParent)
+{
+    std::cout << "basicCrossover";
+    if ((firstParent.size() == 0) || (secondParent.size() == 0))
+    {
+        if ((firstParent.size() == 0) && (secondParent.size() == 0))
+        {
+            std::cout << "The size of both vectors is zero";
+            exit(1);
+        }
+        else
+        {
+            if (firstParent.size() == 0)
+            {
+                std::cout << "The size of the first vector is zero";
+                exit(1);
+            }
+            else
+            {
+                std::cout << "The size of the second vector is zero";
+                exit(1);
+            }
+        }
+    }
+    int divisionPosition = 1 + rand() % firstParent.size();
+    std::vector<std::vector<bool>> variantsDaughterSpecies(2);
+    for (int geneIndex = 0; geneIndex < firstParent.size(); geneIndex++)
+    {
+        if (geneIndex < divisionPosition)
+        {
+            variantsDaughterSpecies.at(0).push_back(firstParent.at(geneIndex));
+            variantsDaughterSpecies.at(1).push_back(secondParent.at(geneIndex));
+        }
+        else
+        {
+            variantsDaughterSpecies.at(0).push_back(secondParent.at(geneIndex));
+            variantsDaughterSpecies.at(1).push_back(firstParent.at(geneIndex));
+        }
+    }
+
+    return variantsDaughterSpecies;
+}
